b66882d63f89723c9e9340259ba2d8ea6fa2801b
JDCG00 <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-04-07 09:33:46 +0200

    Comentado v1.

--- 3.POO/v1/inicio.php
+++ 3.POO/v1/inicio.php
@@ -1,9 +1,12 @@
 <?php
-
+    //Se crea la clase Inicio para mandar las variables con mayor seguridad.
     class Inicio{
+        //Creación del atributo $meses
         private $meses; 
 
+        //Creación del método añadir, que crea el array en el atributo $meses
         public function añadir(){
+            //Se crea el array en el atributo $meses
             $this -> meses = array(
                 "<b>Enero</b>" => 31,
                 "<b>Febrero</b>" => 28,
@@ -19,10 +22,13 @@
                 "<b>Diciembre</b>" => 30
             );
         }
+        //Creación del método mostrar, el cual visualiza los datos creados en el array $meses.
         public function mostrar(){
             
+            //Creo una variable $arrayMeses que contiene el atributo creado anteriormente, para que sea más intuitivo y más accesible utilizar el atributo dentro de la función mostrar. Esta variable solo se puede utilizar de manera local única y exlusivamente en este método.
             $arrayMeses = $this -> meses;
         
+            //Recorro el array $arrayMeses(qué es el atributo $meses) e imprimo el índice y el valor de cada elemento recorrido del array.
             foreach ($arrayMeses as $indice => $valor) {
                 echo "$indice &nbsp $valor <br>";
             }
