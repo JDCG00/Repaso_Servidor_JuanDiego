750f62cdb0ea7e6078538fe0a3017d0bf80757f8
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-04-23 17:07:54 +0200

    Script Permisos Usuario.

--- 4.Minijuegos/sql/permisosUsuario.sql
+++ 4.Minijuegos/sql/permisosUsuario.sql
@@ -1,8 +1,5 @@
 /*Se crea el usuario*/
-CREATE USER 'test'@'localhost' IDENTIFIED VIA mysql_native_password USING '***';GRANT USAGE ON *.* TO 'test'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0; 
-
-/*Se le quita todos los permisos globales que tenga, y se le da premisos para manejo de datos(selecciona, insertar, actualizar y borrar)*/
-REVOKE ALL PRIVILEGES ON *.* FROM 'test'@'localhost'; REVOKE GRANT OPTION ON *.* FROM 'test'@'localhost'; GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'test'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0; 
+CREATE USER 'test'@'localhost' IDENTIFIED VIA mysql_native_password USING '***';GRANT USAGE ON *.* TO 'test'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 
 /*Se le da permisos al usuario test sobre la base de datos Minijuegos_Repaso, para que solo pueda hacer manejo de datos(seleccionar, insertar, acutalizar y borrar) */
 GRANT SELECT, INSERT, UPDATE, DELETE ON `Minijuegos\_Repaso`.* TO 'test'@'localhost';
