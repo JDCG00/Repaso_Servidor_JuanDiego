33d6f962c09313445f4cff525cade5fd1a68c1d4
JDCG00 <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-04-26 17:06:08 +0200

    Script permisos usuario.

--- 4.Minijuegos/sql/permisosUsuario.sql
+++ 4.Minijuegos/sql/permisosUsuario.sql
@@ -2,4 +2,4 @@
 CREATE USER 'test'@'localhost' IDENTIFIED VIA mysql_native_password USING '***';GRANT USAGE ON *.* TO 'test'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 
 /*Se le da permisos al usuario test sobre la base de datos Minijuegos_Repaso, para que solo pueda hacer manejo de datos(seleccionar, insertar, acutalizar y borrar) */
-GRANT SELECT, INSERT, UPDATE, DELETE ON `Minijuegos\_Repaso`.* TO 'test'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `minijuegos\_Repaso`.* TO 'test'@'localhost';
