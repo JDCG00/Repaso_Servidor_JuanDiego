a1680fb286976eb73c69a0925590c19e8ba9d365
JDCG00 <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-06-06 12:43:51 +0200

    test

--- 4.Minijuegos/php/config_db.php
+++ 4.Minijuegos/php/config_db.php
@@ -1,7 +1,14 @@
 <?php
     // /* Se definen las constantes para conectar con la base de datos del equipo local*/
-    define('SERVIDOR', 'localhost');
-    define('USUARIO', 'test');
-    define('PW', 'hola1234');
-    define('BD', 'minijuegos_Repaso');
+    // define('SERVIDOR', 'localhost');
+    // define('USUARIO', 'test');
+    // define('PW', 'hola1234');
+    // define('BD', 'minijuegos_Repaso');
+
+
+    /* Se definen las constantes para conectar con la base de datos del servidor*/
+    define('SERVIDOR', '2daw.esvirgua.com');
+    define('USUARIO', 'user2daw_14');
+    define('PW', 'i;?]2Gc*KGWQ');
+    define('BD', 'user2daw_BD2-14');
 ?>
